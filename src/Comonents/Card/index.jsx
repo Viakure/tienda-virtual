00e1e7ff32324a5a492c0f5fe25c655f510573e1
Viakure <nicolas.ipinzam@utem.cl>
2023-11-20 18:07:50 -0300

    Seleccion unica de producto y se pueden eliminar elementos del carrito

--- src/Comonents/Card/index.jsx
+++ src/Comonents/Card/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { PlusIcon } from '@heroicons/react/24/solid'
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Card = (data) => {
@@ -10,7 +10,7 @@ const Card = (data) => {
         context.setProductToShow(productData);
     };
 
-    const addProductToCart = (event, productData) =>{
+    const addProductToCart = (event, productData) => {
         event.stopPropagation();
         context.setCount(context.count + 1);
         context.setCartProducts([...context.cartProducts, productData]);
@@ -18,16 +18,34 @@ const Card = (data) => {
         context.closeProductDetail();
     };
 
+    const renderIcon = (id) => {
+        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0;
+
+        if (isInCart) {
+            return (
+                <button
+                    className="absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1"
+                ><CheckIcon className="h-6 w-6 text-white" />
+                </button>
+            )
+        }else{
+            return (
+                <button
+                    className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+                    onClick={(event) => addProductToCart(event, data.data)}
+                ><PlusIcon className="h-6 w-6 text-black" />
+                </button>
+            )
+        };
+        
+    };
+
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounded-lg" onClick={() => showProduct(data.data)}>
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={data.data.image} alt={data.data.title} />
-                <button
-                className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-                onClick={(event) => addProductToCart(event, data.data)}
-                ><PlusIcon className="h-6 w-6 text-black"/>
-                </button>
+                {renderIcon(data.data.id)}
             </figure>
             <p className="flex justify-between">
                 <span className="text-sm font-light">{data.data.title}</span>
--- src/Comonents/CheckoutSideMenu/index.jsx
+++ src/Comonents/CheckoutSideMenu/index.jsx
@@ -6,7 +6,10 @@ import './styles.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext);
-    console.log('Cart: ', context.cartProducts);
+    const handleDelete = (id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id);
+        context.setCartProducts(filteredProducts);
+    };
     return(
         <aside className={` ${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-1 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center'>
@@ -14,14 +17,16 @@ const CheckoutSideMenu = () => {
                 <XMarkIcon className="h-6 w-6 text-black fixed right-3 cursor-pointer" onClick={() => context.closeCheckoutSideMenu()}/>
 
             </div>
-            <div className='px-6'>
+            <div className='px-6 overflow-y-scroll'>
             {
                 context.cartProducts.map(product => (
                     <OrderCard
                     key={product.id}
+                    id={product.id}
                     title={product.title}
                     imageUrl={product.image}
                     price={product.price}
+                    handleDelete={handleDelete}
                     />
                 ))
             }
