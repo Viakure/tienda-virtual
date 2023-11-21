4cf264cd244704ffc46e43e8622ead4f1dc918aa
Viakure <nicolas.ipinzam@utem.cl>
2023-11-21 18:43:32 -0300

    Agregando ultima orden realizada

--- src/Comonents/CheckoutSideMenu/index.jsx
+++ src/Comonents/CheckoutSideMenu/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { XMarkIcon} from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context';
 import { OrderCard } from '../OrderCard';
@@ -48,7 +49,9 @@ const CheckoutSideMenu = () => {
                     <span className='font-light'>Total:</span>
                     <span className='font-medium text-lg'>${totalPrice(context.cartProducts)}</span>
                 </p>
-                <button className='w-full bg-black py-3 text-white rounded ' onClick={() => handleCheckout()}>Checkout</button>
+                <Link to='/MyOrders/last'>
+                    <button className='w-full bg-black py-3 text-white rounded ' onClick={() => handleCheckout()}>Checkout</button>
+                </Link>
             </div>
         </aside>
     )
--- src/Comonents/OrderCard/index.jsx
+++ src/Comonents/OrderCard/index.jsx
@@ -1,8 +1,12 @@
-import { XMarkIcon} from '@heroicons/react/24/solid'
+import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = props => {
     const { id, title, imageUrl, price, handleDelete } = props
-    return(
+    let renderXmarkIcon;
+    if (handleDelete) {
+        renderXmarkIcon = <XMarkIcon className="h-6 w-6 text-black cursor-pointer" onClick={() => handleDelete(id)} />;
+    }
+    return (
         <div className="flex justify-between items-center py-2">
             <div className='flex items-center gap-2'>
                 <figure className='w-20 h-20'>
@@ -12,7 +16,7 @@ const OrderCard = props => {
             </div>
             <div className='flex items-center gap-2'>
                 <p className='text-lg font-medium px-2'>${price}</p>
-                <XMarkIcon className="h-6 w-6 text-black cursor-pointer" onClick={() => handleDelete(id)}/>
+                {renderXmarkIcon}
             </div>
         </div>
     )
