0fda02ce1aff71138c4b1cd55f2e958a0ab6387f
Viakure <nicolas.ipinzam@utem.cl>
2023-11-27 17:03:00 -0300

    Creada la card de my orders y el flujo de order a orders

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from "../../Context";
 import { OrderCard } from "../../Comonents/OrderCard";
 import { Layout } from "../../Comonents/Layout";
@@ -7,7 +9,12 @@ function MyOrder() {
     const context = useContext(ShoppingCartContext);
     return (
         <Layout>
-            MyOrder
+            <div className="flex items-center justify-center relative w-80 mb-6">
+                <Link to='/MyOrders' className="absolute left-0">
+                    <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer" />
+                </Link>
+                <h1>My Order</h1>
+            </div>
             <div className='flex flex-col w-80'>
             {
                 context.order?.slice(-1)[0].products.map(product => (
--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,10 +1,25 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { Layout } from "../../Comonents/Layout";
+import { OrdersCard } from "../../Comonents/OrdersCard";
+import { ShoppingCartContext } from "../../Context";
+
 
 function MyOrders() {
+    const context = useContext(ShoppingCartContext);
 
     return (
         <Layout>
-            MyOrders
+            <div className="flex items-center justify-center relative w-80">
+                <h1>My Orders</h1>
+            </div>
+            {
+                context.order.map((order, index) => {
+                    <Link key={index} to={`/MyOrders/${order.id}`}>
+                        <OrdersCard totalPrice={order.totalPrice} totalProducts={order.totalProducts} />
+                    </Link>
+                })
+            }
         </Layout>
     )
 };
